0232a7898533845707f95b13628c0b01106d87fa
Lucas Rogerio da Silva <109930651+Lucas44794@users.noreply.github.com>
2024-08-29 13:42:33 -0300

    alterando

--- escola/views.py
+++ escola/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import auth, messages
 
 def modulos(request):
-    return render(request, 'escola/curso.html')
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'escola/modulos.html')
 
 def curso(request):
-    return render(request, 'escola/visaocurso.html')
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'escola/curso.html')
 
 def logout(request):
     auth.logout(request)
